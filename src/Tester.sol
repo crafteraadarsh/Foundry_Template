e1028335378b7c0dd833d19265acb83f5cc4bb70
Aadarsh Singh <95958161+crafteraadarsh@users.noreply.github.com>
2024-03-10 20:04:38 +0530

    Update Tester.sol

--- src/Tester.sol
+++ src/Tester.sol
@@ -1,14 +1,3 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.19;
+pragma solidity >=0.6.2 <0.9.0;
 
-contract Tester {
-    event Tested(string _data);
-
-    string public data;
-
-    function test(string calldata _data) public payable {
-        data = _data;
-
-        emit Tested(_data);
-    }
-}
